4f0e8de8e6c0b992df3f5ba66783c2cbf4ef3c90
teddycow <fluffgohei@hotmail.com>
2014-04-08 10:03:08 +0200

    Updated FilterScheduler class

--- neutron/plugins/cisco/l3/scheduler/filter_scheduler.py
+++ neutron/plugins/cisco/l3/scheduler/filter_scheduler.py
@@ -1,8 +1,285 @@
 __author__ = 'nalle'
 
+import random
+
+from oslo.config import cfg
+
+from neutron.openstack.common.gettextutils import _
+from neutron.openstack.common import log as logging
+#from neutron.plugins.cisco.l3.scheduler import scheduler_options
+from neutron.plugins.cisco.l3.scheduler import filters
+from neutron.plugins.cisco.l3.scheduler import weights
+
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
+
+
+filter_scheduler_opts = [
+    cfg.IntOpt('scheduler_host_subset_size',
+               default=1,
+               help='New instances will be scheduled on a host chosen '
+                    'randomly from a subset of the N best hosts. This '
+                    'property defines the subset size that a host is '
+                    'chosen from. A value of 1 chooses the '
+                    'first host returned by the weighing functions. '
+                    'This value must be at least 1. Any value less than 1 '
+                    'will be ignored, and 1 will be used instead'),
+
+    cfg.MultiStrOpt('scheduler_available_filters',
+            default=['nova.scheduler.filters.all_filters'],
+            help='Filter classes available to the scheduler which may '
+                    'be specified more than once.  An entry of '
+                    '"nova.scheduler.filters.standard_filters" '
+                    'maps to all filters included with nova.'),
+    cfg.ListOpt('scheduler_default_filters',
+                default=[
+                  'RetryFilter',
+                  'AvailabilityZoneFilter',
+                  'RamFilter',
+                  'ComputeFilter',
+                  'ComputeCapabilitiesFilter',
+                  'ImagePropertiesFilter'
+                  ],
+                help='Which filter class names to use for filtering hosts '
+                      'when not specified in the request.'),
+    cfg.ListOpt('scheduler_weight_classes',
+                default=['nova.scheduler.weights.all_weighers'],
+                help='Which weight class names to use for weighing hosts')
+]
+
+CONF.register_opts(filter_scheduler_opts)
+
+
 class FilterScheduler():
+    """Scheduler that can be used for filtering and weighing."""
+    def __init__(self, *args):
+        super(FilterScheduler, self).__init__(*args)
 
-    def host_router(self):
-        """Schedules non-hosted router(s) on hosting devices."""
+        self.filter_handler = filters.HostFilterHandler()
+        self.filter_classes = self.filter_handler.get_matching_classes(
+                CONF.scheduler_available_filters)
+        self.weight_handler = weights.HostWeightHandler()
+        self.weight_classes = self.weight_handler.get_matching_classes(
+                CONF.scheduler_weight_classes)
 
 
+    def schedule_run_instance(self, context, request_spec,
+                              admin_password, injected_files,
+                              requested_networks, is_first_time,
+                              filter_properties, legacy_bdm_in_spec):
+
+        payload = dict(request_spec=request_spec)
+
+        instance_uuids = request_spec.get('instance_uuids')
+        LOG.info(_("Attempting to build %(num_instances)d instance(s) "
+                    "uuids: %(instance_uuids)s"),
+                  {'num_instances': len(instance_uuids),
+                   'instance_uuids': instance_uuids})
+        LOG.debug(_("Request Spec: %s") % request_spec)
+
+        weighed_hosts = self._schedule(context, request_spec,
+                                       filter_properties, instance_uuids)
+
+        # NOTE: Pop instance_uuids as individual creates do not need the
+        # set of uuids. Do not pop before here as the upper exception
+        # handler fo NoValidHost needs the uuid to set error state
+        instance_uuids = request_spec.pop('instance_uuids')
+
+        # NOTE(comstud): Make sure we do not pass this through.  It
+        # contains an instance of RpcContext that cannot be serialized.
+        filter_properties.pop('context', None)
+
+        for num, instance_uuid in enumerate(instance_uuids):
+            request_spec['instance_properties']['launch_index'] = num
+
+            try:
+                try:
+                    weighed_host = weighed_hosts.pop(0)
+                    LOG.info(_("Choosing host %(weighed_host)s "
+                                "for instance %(instance_uuid)s"),
+                              {'weighed_host': weighed_host,
+                               'instance_uuid': instance_uuid})
+                except IndexError:
+                   # FIX - raise exception.NoValidHost(reason="")
+                    pass
+
+                #TO_DO BIND ROUTER TO VM
+
+            except Exception as ex:
+                # FIX
+                None
+
+            retry = filter_properties.get('retry', {})
+            retry['hosts'] = []
+
+    def _schedule(self, context, request_spec, filter_properties,
+                  instance_uuids=None):
+        """Returns a list of hosts that meet the required specs,
+        ordered by their fitness.
+        """
+        elevated = context.elevated()
+        instance_properties = request_spec['instance_properties']
+        instance_type = request_spec.get("instance_type", None)
+
+        update_group_hosts = self._setup_instance_group(context,
+                filter_properties)
+
+        config_options = self._get_configuration_options()
+
+        # check retry policy.  Rather ugly use of instance_uuids[0]...
+        # but if we've exceeded max retries... then we really only
+        # have a single instance.
+        properties = instance_properties.copy()
+        if instance_uuids:
+            properties['uuid'] = instance_uuids[0]
+        self._populate_retry(filter_properties, properties)
+
+        filter_properties.update({'context': context,
+                                  'request_spec': request_spec,
+                                  'config_options': config_options,
+                                  'instance_type': instance_type})
+
+        self.populate_filter_properties(request_spec,
+                                        filter_properties)
+
+        hosts = None #FIX
+
+        selected_hosts = []
+
+        if instance_uuids:
+            num_instances = len(instance_uuids)
+        else:
+            num_instances = request_spec.get('num_instances', 1)
+        for num in xrange(num_instances):
+            # Filter local hosts based on requirements ...
+            hosts = self.get_filtered_hosts(hosts,
+                    filter_properties, index=num)
+            if not hosts:
+                # Can't get any more locally.
+                break
+
+            LOG.debug(_("Filtered %(hosts)s"), {'hosts': hosts})
+
+            weighed_hosts = self.get_weighed_hosts(hosts,
+                    filter_properties)
+
+            LOG.debug(_("Weighed %(hosts)s"), {'hosts': weighed_hosts})
+
+            scheduler_host_subset_size = CONF.scheduler_host_subset_size
+            if scheduler_host_subset_size > len(weighed_hosts):
+                scheduler_host_subset_size = len(weighed_hosts)
+            if scheduler_host_subset_size < 1:
+                scheduler_host_subset_size = 1
+
+            chosen_host = random.choice(
+                weighed_hosts[0:scheduler_host_subset_size])
+            selected_hosts.append(chosen_host)
+
+            # Now consume the resources so the filter/weights
+            # will change for the next instance.
+            chosen_host.obj.consume_from_instance(instance_properties)
+            if update_group_hosts is True:
+                filter_properties['group_hosts'].add(chosen_host.obj.host)
+        return selected_hosts
+
+    def _choose_host_filters(self, filter_cls_names):
+        """Since the caller may specify which filters to use we need
+        to have an authoritative list of what is permissible. This
+        function checks the filter names against a predefined set
+        of acceptable filters.
+        """
+        if filter_cls_names is None:
+            filter_cls_names = CONF.scheduler_default_filters
+        if not isinstance(filter_cls_names, (list, tuple)):
+            filter_cls_names = [filter_cls_names]
+        cls_map = dict((cls.__name__, cls) for cls in self.filter_classes)
+        good_filters = []
+        bad_filters = []
+        for filter_name in filter_cls_names:
+            if filter_name not in cls_map:
+                bad_filters.append(filter_name)
+                continue
+            good_filters.append(cls_map[filter_name])
+        if bad_filters:
+            msg = ", ".join(bad_filters)
+           # FIX raise exception.SchedulerHostFilterNotFound(filter_name=msg)
+        return good_filters
+
+    def get_filtered_hosts(self, hosts, filter_properties,
+            filter_class_names=None, index=0):
+        """Filter hosts and return only ones passing all filters."""
+
+        def _strip_ignore_hosts(host_map, hosts_to_ignore):
+            ignored_hosts = []
+            for host in hosts_to_ignore:
+                for (hostname, nodename) in host_map.keys():
+                    if host == hostname:
+                        del host_map[(hostname, nodename)]
+                        ignored_hosts.append(host)
+            ignored_hosts_str = ', '.join(ignored_hosts)
+            msg = _('Host filter ignoring hosts: %s')
+            LOG.audit(msg % ignored_hosts_str)
+
+        def _match_forced_hosts(host_map, hosts_to_force):
+            forced_hosts = []
+            for (hostname, nodename) in host_map.keys():
+                if hostname not in hosts_to_force:
+                    del host_map[(hostname, nodename)]
+                else:
+                    forced_hosts.append(hostname)
+            if host_map:
+                forced_hosts_str = ', '.join(forced_hosts)
+                msg = _('Host filter forcing available hosts to %s')
+            else:
+                forced_hosts_str = ', '.join(hosts_to_force)
+                msg = _("No hosts matched due to not matching "
+                        "'force_hosts' value of '%s'")
+            LOG.audit(msg % forced_hosts_str)
+
+        def _match_forced_nodes(host_map, nodes_to_force):
+            forced_nodes = []
+            for (hostname, nodename) in host_map.keys():
+                if nodename not in nodes_to_force:
+                    del host_map[(hostname, nodename)]
+                else:
+                    forced_nodes.append(nodename)
+            if host_map:
+                forced_nodes_str = ', '.join(forced_nodes)
+                msg = _('Host filter forcing available nodes to %s')
+            else:
+                forced_nodes_str = ', '.join(nodes_to_force)
+                msg = _("No nodes matched due to not matching "
+                        "'force_nodes' value of '%s'")
+            LOG.audit(msg % forced_nodes_str)
+
+        filter_classes = self._choose_host_filters(filter_class_names)
+        ignore_hosts = filter_properties.get('ignore_hosts', [])
+        force_hosts = filter_properties.get('force_hosts', [])
+        force_nodes = filter_properties.get('force_nodes', [])
+
+        if ignore_hosts or force_hosts or force_nodes:
+            # NOTE(deva): we can't assume "host" is unique because
+            #             one host may have many nodes.
+            name_to_cls_map = dict([((x.host, x.nodename), x) for x in hosts])
+            if ignore_hosts:
+                _strip_ignore_hosts(name_to_cls_map, ignore_hosts)
+                if not name_to_cls_map:
+                    return []
+            # NOTE(deva): allow force_hosts and force_nodes independently
+            if force_hosts:
+                _match_forced_hosts(name_to_cls_map, force_hosts)
+            if force_nodes:
+                _match_forced_nodes(name_to_cls_map, force_nodes)
+            if force_hosts or force_nodes:
+                # NOTE(deva): Skip filters when forcing host or node
+                if name_to_cls_map:
+                    return name_to_cls_map.values()
+            hosts = name_to_cls_map.itervalues()
+
+        return self.filter_handler.get_filtered_objects(filter_classes,
+                hosts, filter_properties, index)
+
+    def get_weighed_hosts(self, hosts, weight_properties):
+        """Weigh the hosts."""
+        return self.weight_handler.get_weighed_objects(self.weight_classes,
+                hosts, weight_properties)
