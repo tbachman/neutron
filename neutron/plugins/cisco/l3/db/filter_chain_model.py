a736f618659bb312c5deab5022c2d4a1b231cf6e
teddycow <fluffgohei@hotmail.com>
2014-04-10 14:23:50 +0200

    Write/get filter chain to database

---
+++ neutron/plugins/cisco/l3/db/filter_chain_model.py
@@ -0,0 +1,14 @@
+__author__ = 'nalle'
+
+
+import sqlalchemy as sa
+
+from neutron.db import model_base
+from neutron.db import models_v2
+
+
+class FilterChain(model_base.BASEV2, models_v2.HasId):
+
+    __tablename__ = 'filterchain'
+
+    filter_list = sa.Column(sa.String(1023))
