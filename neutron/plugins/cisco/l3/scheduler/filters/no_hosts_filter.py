1b1e39a6a76796effe4c299dcec271d2c662d0bc
teddycow <fluffgohei@hotmail.com>
2014-04-29 15:58:41 +0200

    Topo filter

--- neutron/plugins/cisco/l3/scheduler/filters/no_hosts_filter.py
+++ neutron/plugins/cisco/l3/scheduler/filters/no_hosts_filter.py
@@ -3,7 +3,7 @@ from neutron.plugins.cisco.l3.scheduler.filters import filters_base
 __author__ = 'nalle'
 
 
-class AllHostsFilter(filters_base.BaseHostFilter):
+class NoHostsFilter(filters_base.BaseHostFilter):
 
     run_filter_once_per_request = True
 
