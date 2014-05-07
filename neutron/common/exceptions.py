08610f3f411b59e3a3f9f3b8e3f7207271ae6b73
teddycow <fluffgohei@hotmail.com>
2014-05-07 10:24:45 +0200

    updated exceptions

--- neutron/common/exceptions.py
+++ neutron/common/exceptions.py
@@ -323,3 +323,15 @@ class DeviceIDNotOwnedByTenant(Conflict):
 
 class InvalidCIDR(BadRequest):
     message = _("Invalid CIDR %(input)s given as IP prefix")
+
+
+class NoValidHost(NeutronException):
+    message = _("No valid host was found. %(reason)s")
+
+
+class NoFilterChainFound(NeutronException):
+    message = _(" ")
+
+
+class ClassNotFound(NotFound):
+    message = _("Class %(class_name)s could not be found: %(exception)s")
