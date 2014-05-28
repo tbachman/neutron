08fc3078caa1acaafd4f0d8bc44d87ca633d525e
Bob Melander <bob.melander@gmail.com>
2014-05-28 11:40:20 +0200

    Disabled automatic creation of test networks
    Change-Id: I851c1d1b0869c8ccdcef531096a1b42e1d00674a

--- neutron/plugins/cisco/l3/setup/csr1kv_install_all.sh
+++ neutron/plugins/cisco/l3/setup/csr1kv_install_all.sh
@@ -18,7 +18,8 @@ source ~/devstack/openrc $osn L3AdminTenant
 ./setup_nova_and_glance_for_csr1kv_l3.sh $osn $plugin $localrc $mysql_user $mysql_password
 ./setup_neutron_for_csr1kv_l3.sh $osn $plugin $localrc
 ./setup_l3cfgagent_networking.sh $osn $plugin
-source ~/devstack/openrc admin demo
-./setup_test_networks.sh $osn $plugin
-./setup_interface_on_extnet1_for_demo.sh $osn $plugin
+# Automatic test network creation disabled for now. Can be manually created.
+#source ~/devstack/openrc admin demo
+#./setup_test_networks.sh $osn $plugin
+#./setup_interface_on_extnet1_for_demo.sh $osn $plugin
 
