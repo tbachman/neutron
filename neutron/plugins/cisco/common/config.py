fcfd9733441c039d9d1e2f63e4762210dd61ca45
Abhishek Raut <abhraut@cisco.com>
2014-07-03 08:24:45 -0700

    Make full sync configurable
    Conflicts:
    
    	neutron/plugins/cisco/common/config.py

--- neutron/plugins/cisco/common/config.py
+++ neutron/plugins/cisco/common/config.py
@@ -84,6 +84,8 @@ cisco_n1k_opts = [
                help=_("Number of threads to use to make HTTP requests")),
     cfg.IntOpt('http_timeout', default=30,
                help=_("N1K http timeout duration in seconds")),
+    cfg.BoolOpt('enable_sync_on_start', default=False,
+                help=_("Initiate full sync with VSM on neutron start.")),
     cfg.BoolOpt('restrict_policy_profiles', default=False,
                 help=_("Restrict the visibility of policy profiles to the "
                        "tenants")),
