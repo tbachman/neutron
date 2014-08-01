e272975d3e3ccfd6c52f5eedd3f0c13464d59100
aaronzhang231 <fenzhang@cisco.com>
2014-08-01 16:57:02 -0700

    Implement full sync for cisco n1kv plugin
    Add support to synchronize the state of the neutron database and N1kv VSM(controller)
    in Cisco N1kv neutron plugin
    
    DocImpact
    This commit introduces a new config option enable_sync_on_start in cisco_plugins.ini
    Change-Id: Ic472965a0d402d7d48ae8269c2390f587d02a7e1
    Implements: blueprint cisco-n1kv-full-sync
    
    Conflicts:
    
    	neutron/plugins/cisco/common/config.py
    	neutron/plugins/cisco/n1kv/n1kv_neutron_plugin.py
    	neutron/tests/unit/cisco/n1kv/test_n1kv_plugin.py

--- neutron/plugins/cisco/common/config.py
+++ neutron/plugins/cisco/common/config.py
@@ -87,6 +87,8 @@ cisco_n1k_opts = [
                help=_("N1K http timeout duration in seconds")),
     cfg.IntOpt('http_pool_size', default=4,
                help=_("Number of threads to use to make HTTP requests")),
+    cfg.BoolOpt('enable_sync_on_start', default=False,
+                help=_("Initiate full sync with VSM on neutron start.")),
 ]
 
 cfg.CONF.register_opts(cisco_opts, "CISCO")
