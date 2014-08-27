74163f7604f74c9f15a2ef0b7cafe094acc3a34b
Bob Melander <bob.melander@gmail.com>
2014-08-27 17:15:51 +0200

    Disabled use of host aggregates
    Change-Id: Iae59f4b6a5d2d7c3b1d50bb70aa0fd848a00b9d7

--- neutron/plugins/cisco/l3/setup/setup_nova_and_glance_for_csr1kv_l3.sh
+++ neutron/plugins/cisco/l3/setup/setup_nova_and_glance_for_csr1kv_l3.sh
@@ -27,12 +27,47 @@ networkHostsAggregateName="compute_network_hosts"
 aggregateMetadataKey="aggregate_instance_extra_specs:network_host"
 aggregateMetadataValue="True"
 aggregateMetadata="$aggregateMetadataKey=$aggregateMetadataValue"
-computeNetworkNodes=($(hostname) ComputeNode)
+max_attempts=200
+computeNetworkNodes=( $(hostname) )
 csr1kvImageSrc=$Q_CISCO_CSR1KV_QCOW2_IMAGE
 csr1kvImageName="csr1kv_openstack_img"
 csr1kvDiskFormat="qcow2"
 csr1kvContainerFormat="bare"
 csr1kvGlanceExtraParams="--property hw_vif_model=e1000 --property hw_disk_bus=ide --property hw_cdrom_bus=ide"
+
+
+# We need to add hosts to aggregates in a separate process
+# that can wait for nova compute to start as this script
+# may run before nova compute has started
+function add_host_to_aggregate {
+    local host_array_name=$1[@]
+    local hosts=("${!host_array_name}")
+    local attempt=1
+    echo "Configuring compute nodes to act as network hosts ..."
+    while [ ${#hosts[@]} -gt 0  -a  $attempt -le $max_attempts ]; do
+#        echo "Attempt $attempt out of maximally $max_attempts"
+        for host in "${hosts[@]}"; do
+            host_exists=`nova host-describe $host 2>&1 | awk 'BEGIN { res = "Yes" } /ERROR/ { if ($1 == "ERROR") res = "No"; } END { print res; } '`
+            if [ "$host_exists" == "Yes" ]; then
+                host_added=`nova aggregate-details $aggregateId 2>&1 | awk -v host=$host 'BEGIN { res = "No" } { if (index($8, host) > 0) res = "Yes"; } END { print res }'`
+                if [ "$host_added" == "No" ]; then
+                    echo "    Adding host '$host' to '$networkHostsAggregateName' aggregate"
+                    nova aggregate-add-host $aggregateId $host > /dev/null 2>&1
+#                else
+#                    echo "    Skipping host '$host' since it has already been added"
+                fi
+            else
+#                echo "    Will retry host '$host' which is not up yet"
+                remaining[${#remaining[@]}]=$host
+            fi
+        done
+        hosts=(${remaining[@]})
+        remaining=( )
+        attempt=$(($attempt+1))
+        sleep 1
+    done
+    exit 0
+}
 
 
 tenantId=`keystone tenant-get $l3AdminTenant 2>&1 | awk '/No tenant|id/ { if ($1 == "No") print "No"; else print $4; }'`
@@ -67,7 +102,9 @@ else
    echo " Yes, it does."
 fi
 
-if [ "$flavorId" != "NO SERVER" ]; then
+# We disable scheduling by aggregate metadata for now.
+if false; then
+#if [ "$flavorId" != "NO SERVER" ]; then
     echo -n "Checking if flavor '$csr1kvFlavorName' has metadata '$aggregateMetadata' ..."
     hasMetadata=`nova flavor-show 621 2>&1 | awk -v key=$aggregateMetadataKey -v value=$aggregateMetadataValue '
     BEGIN { res = "No" }
@@ -83,7 +120,6 @@ if [ "$flavorId" != "NO SERVER" ]; then
     else
        echo " Yes, it does."
     fi
-
 
     echo -n "Checking if aggregate '$networkHostsAggregateName' exists ..."
     aggregateId=`nova aggregate-list 2>&1 | awk -v name=$networkHostsAggregateName -v r=$networkHostsAggregateName"|Id" '
@@ -104,24 +140,12 @@ if [ "$flavorId" != "NO SERVER" ]; then
     echo "Setting metadata for aggregate '$networkHostsAggregateName'"
     nova aggregate-set-metadata $aggregateId $aggregateMetadata > /dev/null 2>&1
 
-    echo "Configuring compute nodes to act as network hosts ..."
+    # Add nodes to the aggregate in a separate process that can run until
+    # the nova compute has started on the hosts.
+    ( add_host_to_aggregate computeNetworkNodes ) &
+fi
 
-    for host in ${computeNetworkNodes[*]}
-    do
-       host_exists=`nova host-describe $host 2>&1 | awk 'BEGIN { res = "Yes" } /ERROR/ { if ($1 == "ERROR:") res = "No"; } END { print res; } '`
-       if [ "$host_exists" == "Yes" ]; then
-           host_added=`nova aggregate-details $aggregateId 2>&1 | awk -v host=$host 'BEGIN { res = "No" } { if (index($8, host) > 0) res = "Yes"; } END { print res }'`
-           if [ "$host_added" == "No" ]; then
-               echo "    Adding host '$host' to '$networkHostsAggregateName' aggregate"
-               nova aggregate-add-host $aggregateId $host > /dev/null 2>&1
-           else
-               echo "    Skipping host '$host' since it has already been added"
-           fi
-       else
-           echo "    Skipping host '$host' which is not up"
-       fi
-    done
-
+if [ "$flavorId" != "NO SERVER" ]; then
     echo "Removing relevant quota limits ..."
     nova quota-update --cores -1 --instances -1 --ram -1 $tenantId > /dev/null 2>&1
 fi
