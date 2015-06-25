ba678acc4d19ff5adf5247c855c02c8a0dd5061a
Nelson Huang <nelson.huang@onecloudinc.com>
2015-06-25 22:41:35 +0000

    Added some comments and removed unused method

--- neutron/plugins/cisco/db/l3/l3_router_appliance_db.py
+++ neutron/plugins/cisco/db/l3/l3_router_appliance_db.py
@@ -53,6 +53,7 @@ from neutron.plugins.common import constants as svc_constants
 LOG = logging.getLogger(__name__)
 
 import pprint
+import rpdb
 
 AGENT_TYPE_L3 = l3_constants.AGENT_TYPE_L3
 AGENT_TYPE_L3_CFG = cisco_constants.AGENT_TYPE_L3_CFG
@@ -265,6 +266,7 @@ class L3RouterApplianceDBMixin(extraroute_db.ExtraRoute_dbonly_mixin):
                  r_hd_binding_db.router_type_id !=
                  self.get_namespace_router_type_id(context))
         if is_ha:
+            rpdb.set_trace(addr="127.0.0.1", port=4444)
             # Ensure update is compliant with any HA
             req_ha_settings = self._ensure_update_ha_compliant(
                 context, router_id, r)
