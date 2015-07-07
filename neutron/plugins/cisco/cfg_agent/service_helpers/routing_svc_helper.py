d85b167a3b12f26f48371cf0fb9f5d66cf744d04
jw <jon.wei@onecloudinc.com>
2015-07-06 17:57:44 -0700

    Remove unsupported parameter from driver.router_removed call

--- neutron/plugins/cisco/cfg_agent/service_helpers/routing_svc_helper.py
+++ neutron/plugins/cisco/cfg_agent/service_helpers/routing_svc_helper.py
@@ -616,7 +616,8 @@ class RoutingServiceHelper(object):
             if deconfigure:
                 self._process_router(ri)
                 driver = self._drivermgr.get_driver(router_id)
-                driver.router_removed(ri, deconfigure)
+                #driver.router_removed(ri, deconfigure)
+                driver.router_removed(ri)
                 self._drivermgr.remove_driver(router_id)
             del self.router_info[router_id]
             self.removed_routers.discard(router_id)
