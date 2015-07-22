80714517d5116fe71cb2af1cb8bb58951b36b3cf
jw <jon.wei@onecloudinc.com>
2015-07-22 15:54:57 -0700

    Add loader module for automated asr1k cfg check, in progress

---
+++ neutron/plugins/cisco/cfg_agent/device_drivers/asr1k/asr1k_auto_config_check.py
@@ -0,0 +1,66 @@
+import sys
+
+import ciscoconfparse
+import ncclient
+
+import oslo_messaging
+from oslo_config import cfg
+
+from neutron.common import rpc as n_rpc
+from neutron.common import topics
+from neutron.common import config as common_config
+
+
+class CiscoDevMgrRPC(object):
+    """Agent side of the device manager RPC API."""
+
+    def __init__(self, topic, host):
+        self.host = host
+        target = oslo_messaging.Target(topic=topic, version='1.0')
+        self.client = n_rpc.get_client(target)
+
+    def get_active_hosting_devices(self, context):
+        """Get a list of all active hosting devices."""
+        cctxt = self.client.prepare()
+        return cctxt.call(context,
+                          'get_active_hosting_devices',
+                          host=self.host)
+
+class CiscoRoutingPluginRPC(object):
+    """RoutingServiceHelper(Agent) side of the  routing RPC API."""
+
+    def __init__(self, topic, host):
+        self.host = host
+        target = oslo_messaging.Target(topic=topic, version='1.0')
+        self.client = n_rpc.get_client(target)
+
+    def get_routers(self, context, router_ids=None, hd_ids=None):
+        """Make a remote process call to retrieve the sync data for routers.
+
+        :param context: session context
+        :param router_ids: list of  routers to fetch
+        :param hd_ids : hosting device ids, only routers assigned to these
+                        hosting devices will be returned.
+        """
+        cctxt = self.client.prepare()
+        return cctxt.call(context, 'cfg_sync_routers', host=self.host,
+                          router_ids=router_ids, hosting_device_ids=hd_ids)
+
+def main():
+
+    conf = cfg.CONF
+
+    common_config.init(sys.argv[1:])
+    conf(project='neutron')
+
+    host = conf.host
+    devmgr_rpc = CiscoDevMgrRPC(topics.DEVICE_MANAGER_PLUGIN, host)
+    plugin_rpc = CiscoRoutingPluginRPC(topics.L3PLUGIN, host)
+
+    context = {}
+    hosting_devs = devmgr_rpc.get_active_hosting_devices(context)
+    for hd in hosting_devs:
+        print("HOSTING DEVICE: %s" % hd)
+
+if __name__ == "__main__":
+    main()
